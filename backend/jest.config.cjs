6337aecb52170634454f8ddc4652eac46dac2979
Samuel Alvarez <samuel.alvarezbecerra.57@gmail.com>
2026-03-17 10:00:36 -0500

    fix: arreglar errores en test

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -2,11 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
